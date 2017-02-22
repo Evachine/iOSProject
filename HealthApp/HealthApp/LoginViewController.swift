2cadb9250697b6e8c48d184c8ff7bf107e571869
Evan Joseph Hench <Evachine@Evans-MacBook-Pro.local>
2017-02-22 10:51:10 -0800

    Updated Login, Signup, and DoneSigningUp buttons and segues

--- HealthApp/HealthApp/LoginViewController.swift
+++ HealthApp/HealthApp/LoginViewController.swift
@@ -8,101 +8,106 @@
 
 import UIKit
 import Firebase
-import GoogleSignIn
+//import GoogleSignIn
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
-
-   @IBOutlet weak var emailText: UITextField!
-   @IBOutlet weak var passwordText: UITextField!
-   
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-      emailText.delegate = self
-      passwordText.delegate = self
-      
-      passwordText.isSecureTextEntry = true
-
+        
+        emailText.delegate = self
+        passwordText.delegate = self
+        
+        passwordText.isSecureTextEntry = true
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-   
-   func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-      return true
-   }
-   
-   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-      if (textField == emailText) {
-         print("Email: \(textField.text)")
-         
-         emailText.text = textField.text
-      }
-      
-      if (textField == passwordText) {
-         print("PW: \(textField.text)")
-         
-         passwordText.text = textField.text
-      }
-      
-      textField.resignFirstResponder()
-      
-      return true
-   }
-
-   
-   @IBAction func loginButtonPressed(_ sender: UIButton) {
-      
-      print(emailText.text!)
-      print(passwordText.text!)
-      
-      
-
-      
-   }
-   
-   
-   @IBAction func signUpButtonPressed(_ sender: UIButton) {
-   }
-   
-   
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == emailText) {
+            print("Email: \(textField.text)")
+            
+            emailText.text = textField.text
+        }
+        
+        if (textField == passwordText) {
+            print("PW: \(textField.text)")
+            
+            passwordText.text = textField.text
+        }
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+        print(emailText.text!)
+        print(passwordText.text!)
+        
+        // TODO: Add functionality!
+        
+        performSegue(withIdentifier: "loginIsPressed", sender: sender)
+        
+        
+    }
+    
+    
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        // TODO: Add functionality!
+        
+        performSegue(withIdentifier: "signupIsPressed", sender: sender)
+    }
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-   
-   /* Firebase crap:
-    
-    To create a new user with the passed in info: 
-    
-    FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
-    // ...
-    }
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     
-    To sign in:
-    FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
-    // ...
-    }
-    
-    To sign out:
-    
-    let firebaseAuth = FIRAuth.auth()
-    do {
-    try firebaseAuth?.signOut()
-    } catch let signOutError as NSError {
-    print ("Error signing out: %@", signOutError)
-    }
-    
-    
-    */
+    /* Firebase crap:
+     
+     To create a new user with the passed in info:
+     
+     FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+     // ...
+     }
+     
+     
+     To sign in:
+     FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+     // ...
+     }
+     
+     To sign out:
+     
+     let firebaseAuth = FIRAuth.auth()
+     do {
+     try firebaseAuth?.signOut()
+     } catch let signOutError as NSError {
+     print ("Error signing out: %@", signOutError)
+     }
+     
+     
+     */
 }
---
+++ HealthApp/HealthApp/SignUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignUpViewController.swift
+//  HealthApp
+//
+//  Created by Emily K. Nguyen on 2/14/17.
+//  Copyright © 2017 Emily K. Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneSigningUpPressed(_ sender: UIButton) {
+        // TODO: Add functionality!
+        
+        performSegue(withIdentifier: "doneSigningUpPressed", sender: sender)
+        
+        
+    }
+    
+}
+
