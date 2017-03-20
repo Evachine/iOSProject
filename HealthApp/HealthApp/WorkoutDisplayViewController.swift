e339506b8a99e1531d649f427e74d6e4867e69ae
Evan Joseph Hench <Evachine@Evans-MacBook-Pro.local>
2017-03-20 10:38:36 -0700

    Added SwiftyJSON

--- HealthApp/HealthApp/WorkoutDisplayViewController.swift
+++ HealthApp/HealthApp/WorkoutDisplayViewController.swift
@@ -16,23 +16,53 @@ class WorkoutDisplayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let html = "<a id=1 href='?foo=bar&mid&lt=true'>One</a> <a id=2 href='?foo=bar&lt;qux&lg=1'>Two</a>"
-        
-        if let doc = HTML(html: html, encoding: .utf8) {
-            print(doc.title!)
-            
-            // Search for nodes by CSS
-            for link in doc.css("a, link") {
-                print(link.text!)
-                print(link["href"]!)
+        if let url = URL(string: "http://www.73summits.com/ergdb/api/workout/3198/292/json") {
+            let session = URLSession.shared
+            let download = session.dataTask(with: url) {
+                (data: Data?, response: URLResponse?, error: Error?) -> Void in
+                
+                if let data = data {
+                    print("DATA:")
+                    print(data)
+                    print("----")
+                    let json = JSON(data:data)
+                    print("JSON:")
+                    print(json)
+                    print("----")
+                    
+//                    DispatchQueue.main.async {
+//                        [weak self] in
+//                        guard let this = self else { return }
+//                        this.posLabel.text = json["features"][0]["properties"]["place"].stringValue
+//                        this.tableButton.isEnabled = true
+//                        this.features = json["features"]
+//                    }
+                }
             }
             
-            // Search for nodes by XPath
-            for link in doc.xpath("//a | //link") {
-                print(link.text!)
-                print(link["href"]!)
-            }
+            download.resume()
         }
+        
+        
+//        let myURLString = "http://rpmtraining.com/blog/"
+//        var html = ""
+//        guard let myURL = URL(string: myURLString) else {
+//            print("Error: \(myURLString) doesn't seem to be a valid URL")
+//            return
+//        }
+//        
+//        do {
+//            html = try String(contentsOf: myURL, encoding: .ascii)
+//            print("HTML : \(html)")
+//            
+//            // Titles are in an <h2 class="entry-title"...
+//            //
+//            // Content is in a <p><strong>CROSSFIT...
+//            // CSPORT content is same but it's <p class="p1"><strong>CSPORT......
+//        }
+//        catch let error {
+//            print("Error: \(error)")
+//        }
     }
     
     override func didReceiveMemoryWarning() {
